421a8b755b69fb1a2282910c1dcf2da45567f3c2
ChrisFong604 <ccffoonngg@gmail.com>
2024-06-22 22:06:37 -0700

    added dummy inputs for user creation, and userslist component to display users

--- src/app/components/UsersList.tsx
+++ src/app/components/UsersList.tsx
@@ -2,11 +2,25 @@
 import { trpc } from "@/trpc/client";
 
 export default function UsersList() {
-  const users = trpc.usersRouter.getUsers.useQuery();
+  const users = trpc.users.getUsers.useQuery().data;
 
   return (
     <div>
-      <div>{JSON.stringify(users.data)}</div>
+      <div>
+        {users?.map((user, key) => {
+          return (
+            <div>
+              <strong>
+                {user.last_name}, {user.first_name}
+              </strong>
+              <ul>
+                <li>{user.email}</li>
+              </ul>
+              <br></br>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,56 @@
 "use client";
+import { useState } from "react";
 import { trpc } from "../trpc/client";
-
-const hardcodeUser = {
-  email: "testEmail",
-  first_name: "firstname",
-  last_name: "lastname",
-};
+import UsersList from "./components/UsersList";
 
 export default function Home() {
   const appHealthCheck = trpc.health_check.useQuery();
+  const addUser = trpc.users.addUser.useMutation();
+
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
 
   return (
     <>
       <div>Hacker Portal</div>
       <br></br>
       <div>App Health Check: {appHealthCheck.data}</div>
+      <br></br>
+      <hr />
+      <h2 className="font-bold">Users</h2>
+      <UsersList />
+      <hr />
+      <br></br>
+      <br></br>
+
+      <input
+        type="text"
+        onChange={(e) => setFirstName(e.target.value)}
+        placeholder="first name"
+      ></input>
+      <input
+        type="text"
+        onChange={(e) => setLastName(e.target.value)}
+        placeholder="last name"
+      ></input>
+      <input
+        type="text"
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="email"
+      ></input>
+
+      <button
+        onClick={async () => {
+          addUser.mutate({
+            email: email,
+            first_name: firstName,
+            last_name: lastName,
+          });
+        }}
+      >
+        Add a new user
+      </button>
     </>
   );
 }
