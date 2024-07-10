15d35b0a2f7b51629f2ba4d5ca00523d4e2ee80b
Jenna Lee <jennaolee03@gmail.com>
2024-07-10 12:46:42 -0700

    Added delete hackthon button

--- src/db/schema/hackathons.ts
+++ src/db/schema/hackathons.ts
@@ -1,5 +1,6 @@
 import { mysqlTable, varchar, date } from "drizzle-orm/mysql-core";
 import { createId } from "@paralleldrive/cuid2";
+import { z } from "zod";
 
 import { createInsertSchema, createSelectSchema } from "drizzle-zod";
 
@@ -18,6 +19,10 @@ const insertHackathonSchema = createInsertSchema(hackathons, {
     end_date: (schema) => schema.end_date.date(),
 }).omit({ hackathon_id: true });
 
+const deleteHackathonSchema = z.object({
+    hackathon_id: z.string().min(1),
+});
+
 const selectHackathonSchema = createSelectSchema(hackathons);
 
-export { hackathons, insertHackathonSchema, selectHackathonSchema };
+export { hackathons, insertHackathonSchema, selectHackathonSchema, deleteHackathonSchema };
