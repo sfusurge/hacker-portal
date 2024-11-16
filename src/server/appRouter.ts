6dc4bb8e25a1d239d50f2ea65db1c9201ea7184d
brendanshen24 <91840687+brendanshen24@users.noreply.github.com>
2024-11-16 13:54:50 -0800

    integrated nodemailer functionality

--- src/server/appRouter.ts
+++ src/server/appRouter.ts
@@ -2,6 +2,7 @@ import { createCallerFactory, publicProcedure, router } from './trpc';
 
 import { usersRouter } from './routers/usersRouter';
 import { hackathonsRouter } from './routers/hackathonsRouter';
+import {sendEmailRouter} from './routers/sendEmailRouter';
 
 export const appRouter = router({
   health_check: publicProcedure.query(() => {
@@ -10,6 +11,7 @@ export const appRouter = router({
 
   users: usersRouter,
   hackathons: hackathonsRouter,
+  emails: sendEmailRouter,
 });
 
 // For server side call in unit test
