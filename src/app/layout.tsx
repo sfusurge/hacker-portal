cd6565dfc84fa6c90efb992e020535784be77675
ChrisFong604 <ccffoonngg@gmail.com>
2024-11-17 22:15:14 -0800

    created auth button for signin/signout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import Provider from '@/trpc/Provider';
 import NextAuthSessionProvider from './components/auth/NextAuthSessionProvider';
+import AuthButton from './components/auth/AuthButton';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <NextAuthSessionProvider>
+          <AuthButton />
           <Provider>{children}</Provider>
         </NextAuthSessionProvider>
       </body>
