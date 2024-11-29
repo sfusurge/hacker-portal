034bcf1d5ab09b495572a5a99f736d5552248756
goldentoaste <rayg2375@gmail.com>
2024-11-28 17:27:33 -0800

    lineartimeline item onlcik

--- src/components/calendar/LinearTimeline.tsx
+++ src/components/calendar/LinearTimeline.tsx
@@ -1,7 +1,7 @@
 import { CalendarEventType } from './types';
 import moment from 'moment';
 import style from './LinearTimeline.module.css';
-import { CSSProperties, Fragment } from 'react';
+import { CSSProperties, Fragment, useRef, useState } from 'react';
 import { groupEventsByDay } from './MonthCalendarShared';
 export function LinearTimeline({
     events,
@@ -43,6 +43,8 @@ function TimelineDateHeader({ date }: { date: moment.Moment }) {
 }
 
 function TimelineItem({ event }: { event: CalendarEventType }) {
+    const [contentHeight, setContentHeight] = useState(0);
+    const innerContentRef = useRef<HTMLDivElement | null>(null);
     return (
         <div
             className={style.timelineItemWrapper}
@@ -51,15 +53,40 @@ function TimelineItem({ event }: { event: CalendarEventType }) {
                     '--color': event.color,
                 } as CSSProperties
             }
+            onClick={() => {
+                console.log('bleh');
+            }}
         >
             <div
                 className={style.timelineItemMainContent}
                 onClick={() => {
-                    console.log('event clicked', event.title);
+                    if (contentHeight === 0) {
+                        setContentHeight(
+                            innerContentRef.current?.scrollHeight!
+                        );
+                    } else {
+                        setContentHeight(0);
+                    }
                 }}
             >
                 <span>{event.title}</span>
                 <span>{moment(event.startTime).format('LT')}</span>
+            </div>
+
+            <div
+                className={style.timelineItemMoreContent}
+                style={{
+                    maxHeight: `${contentHeight}px`,
+                }}
+            >
+                <div
+                    ref={innerContentRef}
+                    style={{
+                        padding: '0.5rem',
+                    }}
+                >
+                    {event.description}
+                </div>
             </div>
         </div>
     );
