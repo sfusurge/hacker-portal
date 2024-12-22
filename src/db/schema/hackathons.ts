80adeafadee02f7b311cfbf873f567f0ccec0da4
goldentoaste <rayg2375@gmail.com>
2024-12-21 16:38:16 -0800

    updated drizzle-zod to 0.6, removed cuid2, removed password fields, remvoed argon2

--- src/db/schema/hackathons.ts
+++ src/db/schema/hackathons.ts
@@ -1,23 +1,20 @@
-import { createId } from '@paralleldrive/cuid2';
-import { pgTable, varchar } from 'drizzle-orm/pg-core';
+import { pgTable, serial, varchar } from 'drizzle-orm/pg-core';
 
 import { createInsertSchema, createSelectSchema } from 'drizzle-zod';
 import { z } from 'zod';
 
 const hackathons = pgTable('hackathons', {
-  id: varchar('hackathon_id', { length: 128 })
-    .primaryKey()
-    .$defaultFn(() => createId()),
+  id: serial('id').primaryKey(),
   name: varchar('name', { length: 255 }).notNull(),
   startDate: varchar('start_date', { length: 255 }).notNull(),
   endDate: varchar('end_date', { length: 255 }).notNull(),
 });
 
 const insertHackathonSchema = createInsertSchema(hackathons, {
-  id: (schema) => schema.id.optional(),
-  startDate: (schema) => schema.startDate.date(),
-  endDate: (schema) => schema.endDate.date(),
-}).omit({ id: true });
+  id: (id) => id.optional(),
+  startDate: (startDate) => startDate.date(),
+  endDate: (endDate) => endDate.date(),
+});
 
 const deleteHackathonSchema = z.object({
   id: z.string().min(1),
--- src/db/schema/users.ts
+++ src/db/schema/users.ts
@@ -1,56 +1,31 @@
-import { createId } from '@paralleldrive/cuid2';
-import { pgTable, varchar } from 'drizzle-orm/pg-core';
+import { pgTable, serial, varchar } from 'drizzle-orm/pg-core';
 import { createInsertSchema, createSelectSchema } from 'drizzle-zod';
 import { z } from 'zod';
-import { hashPassword } from '../utils';
 
 const users = pgTable('users', {
-  id: varchar('id', { length: 128 })
-    .primaryKey()
-    .$defaultFn(() => createId()),
+  id: serial('id').primaryKey(),
   firstName: varchar('first_name', { length: 64 }).notNull(),
   lastName: varchar('last_name', { length: 64 }).notNull(),
   email: varchar('email', { length: 255 }).unique().notNull(),
-  // According to the documentation "The hash length is the length of the hash function output in bytes.
-  // Note that the resulting hash is encoded with Base 64, so the digest will be ~1/3 longer
-  // The default value is 32, which produces raw hashes of 32 bytes or digests of 43 characters."
-  //
-  // We also need to account for the metadata which includes the hashing algorithm, its versions, parameters
-  // and the base64 encoded salt used in the hashing process, which is in total 54 characters
-  password: varchar('password', { length: 54 + 43 }).notNull(),
 });
 
 const insertUserSchema = createInsertSchema(users, {
-  email: (schema) => schema.email.email(),
-  id: (schema) => schema.id.optional(),
-})
-  .omit({ id: true })
-  .transform(async (input) => {
-    input.password = await hashPassword(input.password);
-    return input;
-  });
+  email: (email) => email.email(),
+  id: (id) => id.optional(),
+});
 
-const updateUserSchema = z
-  .object({
-    // always 128 chars -- add constraint
-    // id is not 128 characters for some reason, i get 400 when i try to set length to 128
-    // id: z.string().length(128),
-    id: z.string(),
-    firstName: z.string().optional(),
-    lastName: z.string().optional(),
-    email: z.string().email().optional(),
-    password: z.string().optional(),
-  })
-  .transform(async (input) => {
-    if (typeof input.password === 'undefined') {
-      throw new Error('Password must be defined when updating user details');
-    }
-    input.password = await hashPassword(input.password);
-    return input;
-  });
+const updateUserSchema = z.object({
+  // always 128 chars -- add constraint
+  // id is not 128 characters for some reason, i get 400 when i try to set length to 128
+  // id: z.string().length(128),
+  id: z.string(),
+  firstName: z.string().optional(),
+  lastName: z.string().optional(),
+  email: z.string().email().optional(),
+});
 
 const deleteUserSchema = z.object({
-  id: z.string().min(1),
+  id: z.number().int().min(0),
 });
 
 const selectUserSchema = createSelectSchema(users);
