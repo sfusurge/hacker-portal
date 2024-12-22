0a95308f59a4487448d0083463162d98617a2418
ChrisFong604 <ccffoonngg@gmail.com>
2024-12-21 18:11:28 -0800

    refreshed migrations

--- drizzle/0000_happy_klaw.sql
+++ drizzle/0000_happy_klaw.sql
@@ -1,15 +1,14 @@
 CREATE TABLE IF NOT EXISTS "hackathons" (
-	"hackathon_id" varchar(128) PRIMARY KEY NOT NULL,
+	"id" serial PRIMARY KEY NOT NULL,
 	"name" varchar(255) NOT NULL,
 	"start_date" varchar(255) NOT NULL,
 	"end_date" varchar(255) NOT NULL
 );
 --> statement-breakpoint
 CREATE TABLE IF NOT EXISTS "users" (
-	"id" varchar(128) PRIMARY KEY NOT NULL,
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"first_name" varchar(64) NOT NULL,
 	"last_name" varchar(64) NOT NULL,
 	"email" varchar(255) NOT NULL,
-	"password" varchar(97) NOT NULL,
 	CONSTRAINT "users_email_unique" UNIQUE("email")
 );
