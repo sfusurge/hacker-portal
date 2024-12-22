3b72f648299bee0e4b9206a685f473bed3b2c301
ChrisFong604 <ccffoonngg@gmail.com>
2024-12-21 19:12:48 -0800

    create signin callback and moved hackathons schema back to schema dir root

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,10 +2,24 @@
 
 import NextAuth from 'next-auth/next';
 import { authProviders } from '../authProviders';
+import { databaseClient } from '@/db/client';
+import { eq } from 'drizzle-orm';
+import { users } from '@/db/schema/users';
 
 export const handler = NextAuth({
   secret: process.env.NEXTAUTH_SECRET as string,
   providers: authProviders,
+  callbacks: {
+    async signIn({ user, account, profile, email }) {
+      process.stdout.write('Provider: ' + account?.provider);
+      if (!user.email) {
+        return false;
+      }
+      const oauthEmail = user.email;
+      databaseClient.select().from(users).where(eq(users.email, oauthEmail));
+      return true;
+    },
+  },
 });
 
 export { handler as GET, handler as POST };
