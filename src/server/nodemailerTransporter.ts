46f432ba6f1cb940ebafcfcf9aadf09f1ef7457b
brendanshen24 <91840687+brendanshen24@users.noreply.github.com>
2024-12-22 23:05:03 -0800

    completed QR ticket component and basic implementation of QR code scanner

--- src/server/nodemailerTransporter.ts
+++ src/server/nodemailerTransporter.ts
@@ -1,11 +1,6 @@
 import nodemailer from 'nodemailer';
 const env = process.env;
 
-// Nodemailer transporter, basically configures which platform and account you want to use.
-// Right now, we only have Gmail to test with, but you can't just put in your normal Gmail password.
-// You have to generate a custom one for "unsecured platforms" see https://nodemailer.com/usage/using-gmail/
-// and https://medium.com/@y.mehnati_49486/how-to-send-an-email-from-your-gmail-account-with-nodemailer-837bf09a7628
-
 export const transporter = nodemailer.createTransport({
   host: 'blizzard.mxrouting.net',
   port: 465,
