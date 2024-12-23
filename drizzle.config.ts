a7f1c7957b7d481379a6ffccf30cd55a48c9fb40
ChristopherFong604 <76576876+ChrisFong604@users.noreply.github.com>
2024-12-23 02:50:32 -0800

    Drizzle-orm, Drizzle-kit, Drizzle-zod dependency upgrades (#30)
    * upgrade drizzle-orm, moved hackathons.ts back to schema dir root, updated users schema with new drizzle-zod definitions
    
    * updated drizzle-kit dep
    
    * updated zod type definitions fo users and userpersonaldata schemas, updated usersRouter deleteUser endpoint, reverted drizzle dbcredentials to explicit definition
    
    * regenerated migrations

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from 'drizzle-kit';
 
+const env = process.env;
+
 export default defineConfig({
   schema: './src/db/schema/*',
   out: './drizzle',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.POSTGRES_URL!,
+    host: env.DATABASE_HOST || '127.0.0.1',
+    port: Number(env.DATABASE_PORT) || 5432,
+    user: env.DATABASE_USER || 'root',
+    password: env.DATABASE_PASSWORD || '12345',
+    database: env.DATABASE_NAME || 'portaldb',
+    ssl: false,
   },
   verbose: true,
 });
