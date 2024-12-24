defa03ca9a541a7438300f22f90bb40ab30daf16
ChrisFong604 <ccffoonngg@gmail.com>
2024-12-23 18:36:36 -0800

    refactored signin callback and checkEmailExists middleware

--- src/app/api/auth/middleware.ts
+++ src/app/api/auth/middleware.ts
@@ -2,14 +2,12 @@ import { databaseClient } from '@/db/client';
 import { users } from '@/db/schema/users';
 import { eq } from 'drizzle-orm';
 
-export async function checkEmailExists(loginEmail: string): Promise<Boolean> {
-  const emailMatch = await databaseClient
+export async function checkEmailExists(loginEmail: string): Promise<boolean> {
+  const userQueryResult = await databaseClient
     .select()
     .from(users)
     .where(eq(users.email, loginEmail))
     .execute();
-  if (emailMatch.length === 0) {
-    return false;
-  }
-  return true;
+
+  return userQueryResult.length > 1;
 }
