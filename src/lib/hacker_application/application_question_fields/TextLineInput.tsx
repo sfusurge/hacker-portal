bff6bd9abdf32f7e1a3b09908fff044b3779dabb
goldentoaste <rayg2375@gmail.com>
2025-01-02 18:31:55 -0800

    finish input field

--- src/lib/hacker_application/application_question_fields/TextLineInput.tsx
+++ src/lib/hacker_application/application_question_fields/TextLineInput.tsx
@@ -3,29 +3,79 @@
 import { Label } from '@/components/ui/label';
 import { QuestionTextLineInput } from '../types';
 import { Input } from '@/components/ui/input';
-import { useAtom, type Atom } from 'jotai';
+import { atom, PrimitiveAtom, useAtom, type Atom } from 'jotai';
+import { useCallback, useEffect, useMemo, useRef } from 'react';
 
 export function TextLineInput({
-  dataAtom,
+    dataAtom,
 }: {
-  dataAtom: Atom<QuestionTextLineInput>;
+    dataAtom: PrimitiveAtom<QuestionTextLineInput>;
 }) {
-  const [question, setQuestion] = useAtom(dataAtom);
+    const [question, setQuestion] = useAtom(dataAtom);
 
-  return (
-    <div
-      style={{
-        width: 'fit-content',
-      }}
-    >
-      <Label htmlFor="lineInput">{question.title}</Label>
-      {question.description && <span>{question.description}</span>}
-      <Input
-        id="lineInput"
-        type="text"
-        placeholder={question.placeHolder ?? ''}
-        required={question.required}
-      ></Input>
-    </div>
-  );
+    const inputRef = useRef<HTMLInputElement | null>(null);
+
+    const validate = useCallback(() => {
+        if (inputRef.current && question.validator) {
+            inputRef.current.checkValidity();
+
+            if (inputRef.current.validity.valid) {
+                // if valid then update question value
+                question.value = inputRef.current.value;
+                setQuestion({ ...question });
+            } else {
+                if (question.validator.errorMsg) {
+                    inputRef.current.setCustomValidity(
+                        question.validator.errorMsg
+                    );
+                }
+            }
+        }
+    }, [inputRef.current]);
+
+    // use ReturnType to type of client/window, and Nodejs.
+    const timer = useRef<ReturnType<typeof setTimeout> | undefined>();
+
+    return (
+        <div
+            style={{
+                width: 'fit-content',
+            }}
+        >
+            <Label htmlFor="lineInput">{question.title}</Label>
+            {question.description && <span>{question.description}</span>}
+            <Input
+                ref={inputRef}
+                id="lineInput"
+                type="text"
+                placeholder={question.placeHolder ?? ''}
+                required={question.required}
+                pattern={
+                    question.validator
+                        ? question.validator.pattern
+                        : '[\\s\\S]*'
+                }
+                onKeyDown={(e) => {
+                    if (e.key === 'enter') {
+                        validate();
+                    }
+                }}
+                onBlur={() => {
+                    validate();
+                }}
+                onChange={() => {
+                    // trigger validation after 500ms pause of typing.
+                    if (timer.current !== undefined) {
+                        clearTimeout(timer.current);
+                    }
+
+                    timer.current = setTimeout(() => {
+                        // after 500ms of not typing, validate and disable timer.
+                        validate();
+                        timer.current = undefined;
+                    }, 500);
+                }}
+            ></Input>
+        </div>
+    );
 }
--- src/lib/hacker_application/types.ts
+++ src/lib/hacker_application/types.ts
@@ -1,6 +1,6 @@
 interface Entry {
-  title?: string;
-  description?: string;
+    title?: string;
+    description?: string;
 }
 
 /**
@@ -10,85 +10,85 @@ interface Entry {
  * The server api can reject the request for any reason, so client modifying the question set is not a concern.
  */
 export interface ApplicationData extends Entry {
-  version: number; // version must match, discard the application otherwise. Increment version with every change please.
+    version: number; // version must match, discard the application otherwise. Increment version with every change please.
 
-  hackathonName: string; // should this be hackathon id in table instead?
-  submissionTime?: string;
+    hackathonName: string; // should this be hackathon id in table instead?
+    submissionTime?: string;
 
-  pages: ApplicationPage[];
+    pages: ApplicationPage[];
 }
 
 /**
  * Same info as application, except pages are destructured.
  */
 export interface FlatApplication extends Entry {
-  version: number; // version must match, discard the application otherwise. Increment version with every change please.
+    version: number; // version must match, discard the application otherwise. Increment version with every change please.
 
-  hackathonName: string; // should this be hackathon id in table instead?
-  submissionTime: string;
+    hackathonName: string; // should this be hackathon id in table instead?
+    submissionTime: string;
 
-  questions: ApplicationQuestion[];
+    questions: ApplicationQuestion[];
 }
 
 export interface ApplicationPage extends Entry {
-  questions: ApplicationQuestion[];
+    questions: ApplicationQuestion[];
 }
 
 export type ApplicationQuestion =
-  | QuestionCheckBoxInput
-  | QuestionDatePicker
-  | QuestionTextAreaInput
-  | QuestionTextLineInput
-  | QuestionNumberInput
-  | QuestionMultipleChoice
-  | QuestionSchoolName
-  | QuestionMultipleCheckBox
-  | QuestionNameInput;
+    | QuestionCheckBoxInput
+    | QuestionDatePicker
+    | QuestionTextAreaInput
+    | QuestionTextLineInput
+    | QuestionNumberInput
+    | QuestionMultipleChoice
+    | QuestionSchoolName
+    | QuestionMultipleCheckBox
+    | QuestionNameInput;
 
 interface Question extends Entry {
-  questionId: number; // must be unique to the application.
-  type: string | 'N/A';
-  required?: boolean;
-  role?: string; // optionally specify the "role" of the entry, for auto filling purposes.
+    questionId: number; // must be unique to the application.
+    type: string | 'N/A';
+    required?: boolean;
+    role?: string; // optionally specify the "role" of the entry, for auto filling purposes.
 }
 
 export interface QuestionTextLineInput extends Question {
-  type: 'text-line';
-  placeHolder?: string;
-  value?: string;
-  maxCount?: number;
+    type: 'text-line';
+    placeHolder?: string;
+    value?: string;
+    maxCount?: number;
 
-  validationPatterns?: {
-    pattern: string; //regex pattern
-    errorMsg: string; // message to display if the pattern fails
-  };
+    validator?: {
+        pattern: string; //regex pattern
+        errorMsg: string; // message to display if the pattern fails
+    };
 }
 
 export interface QuestionNameInput extends Question {
-  type: 'name';
-  firstName?: string;
-  lastName?: string;
-  maxCount?: number;
+    type: 'name';
+    firstName?: string;
+    lastName?: string;
+    maxCount?: number;
 }
 
 export interface QuestionTextAreaInput extends Question {
-  type: 'text-area';
-  placeHolder?: string;
-  value?: string;
-  maxCount?: number;
+    type: 'text-area';
+    placeHolder?: string;
+    value?: string;
+    maxCount?: number;
 }
 
 export interface QuestionNumberInput extends Question {
-  type: 'number';
-  placeHolder?: number;
-  value?: number;
-  min?: number;
-  max?: number;
+    type: 'number';
+    placeHolder?: number;
+    value?: number;
+    min?: number;
+    max?: number;
 }
 
 export interface QuestionCheckBoxInput extends Question {
-  type: 'checkbox';
-  value?: boolean;
+    type: 'checkbox';
+    value?: boolean;
 }
 
 /**
@@ -96,30 +96,30 @@ export interface QuestionCheckBoxInput extends Question {
  * In Choices, "data" is the internal data, "name" is whats actually displayed.
  */
 export interface QuestionMultipleChoice extends Question {
-  type: 'multiple-choice';
-  value?: string;
-  choices: { data: string; name: string }[];
+    type: 'multiple-choice';
+    value?: string;
+    choices: { data: string; name: string }[];
 }
 
 export interface QuestionMultipleCheckBox extends Question {
-  type: 'multiple-checkbox';
-  choices: {
-    data: string;
-    name: string;
-    value: boolean;
-  }[];
+    type: 'multiple-checkbox';
+    choices: {
+        data: string;
+        name: string;
+        value: boolean;
+    }[];
 }
 
 /**
  * Auto completes based on user input, from a near infinite list of uni names.
  */
 export interface QuestionSchoolName extends Question {
-  // TODO
-  type: 'school-name';
-  value?: string;
+    // TODO
+    type: 'school-name';
+    value?: string;
 }
 
 export interface QuestionDatePicker extends Question {
-  type: 'date';
-  value?: string;
+    type: 'date';
+    value?: string;
 }
