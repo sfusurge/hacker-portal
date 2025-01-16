94faa1bc9d937baa1e85cfbb0a604475281f45c9
goldentoaste <rayg2375@gmail.com>
2025-01-16 10:28:12 -0800

    wip

--- src/db/schema/users.ts
+++ src/db/schema/users.ts
@@ -1,4 +1,10 @@
-import { boolean, pgEnum, pgTable, uuid, varchar } from 'drizzle-orm/pg-core';
+import {
+  boolean,
+  integer,
+  pgEnum,
+  pgTable,
+  varchar,
+} from 'drizzle-orm/pg-core';
 import {
   createInsertSchema,
   createSelectSchema,
@@ -8,7 +14,8 @@ import { z } from 'zod';
 
 export const providersEnum = pgEnum('provider', ['google', 'github', 'email']);
 const users = pgTable('users', {
-  id: uuid('id').primaryKey().defaultRandom(),
+  id: integer('id').primaryKey().generatedAlwaysAsIdentity({ startWith: 1 }),
+  displayId: varchar('display_id', { length: 6 }).notNull().unique(),
   firstName: varchar('first_name', { length: 64 }),
   lastName: varchar('last_name', { length: 64 }),
   phoneNumber: varchar('phone_number', { length: 15 }),
@@ -21,18 +28,18 @@ const selectUserSchema = createSelectSchema(users);
 
 const insertUserSchema = createInsertSchema(users, {
   email: (email) => email.email(),
-  id: (id) => id.optional(),
-});
+}).extend({});
 
-const updateUserSchema = createUpdateSchema(users).extend({
-  id: z.string().uuid(),
-});
+const updateUserSchema = createUpdateSchema(users);
 
-const deleteUserSchema = z
-  .object({
-    id: z.string().uuid(),
-  })
-  .required();
+const deleteUserSchema = z.union([
+  z.object({
+    id: z.number().int(),
+  }),
+  z.object({
+    displayId: z.string().length(6),
+  }),
+]);
 
 export {
   deleteUserSchema,
--- src/lib/PRNG/LCG.ts
+++ src/lib/PRNG/LCG.ts
@@ -21,4 +21,36 @@ function powerMod(b: number, e: number, m: number) {
   return r;
 }
 
-export function LCG() {}
+function LCG_at_N(n: number, a: number, b: number, m: number, seed: number) {
+  const multi = (seed * powerMod(a, n, m)) % m;
+
+  const p = (a - 1) * m;
+  const incre = (b * ((powerMod(a, n, p) - 1) % p)) / (a - 1);
+
+  return Math.floor(multi + incre) % m;
+}
+
+/**
+ * Hull Dobell theorem
+ * LCG would generate number covering the range of [0-m] iff
+ * 1. m and b are coprime
+ * 2. a - 1 is divisible by all prime factors of m
+ * 3. a - 1 is divisible by 4 if m is divisible by 4.
+ */
+const d6Params = Object.freeze({
+  a: 29, // (4 * 7 + 1) -> a - 1 is disible by 2 and 7
+  b: 97, // co prime with m since 97 is prime
+  m: 917504, // 2**17 * 7, a large 6 digit number that has lots of repeated factors
+  seed: 173429, // any starting seed works
+});
+
+/**
+ * return the nth term the LCG sequence with the above configuration.
+ * For example n could be the sequential id of a user, and result would be their display id.
+ */
+export function getSixDigitId(n: number) {
+  return `${LCG_at_N(n, d6Params.a, d6Params.b, d6Params.m, d6Params.seed)}`.padStart(
+    6,
+    '0'
+  );
+}
