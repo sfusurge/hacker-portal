4ad1a07aa92ecac23eba8b1fab47cffbccc5a35d
goldentoaste <rayg2375@gmail.com>
2025-01-17 15:18:21 -0800

    removed comments

--- src/server/routers/usersRouter.ts
+++ src/server/routers/usersRouter.ts
@@ -27,27 +27,20 @@ export const usersRouter = router({
   }),
   addUser: publicProcedure.input(insertUserSchema).mutation(async (opts) => {
     // create the user, and catch their id
-    console.log('A');
-
     const res = await databaseClient
       .insert(users)
       .values({
         ...opts.input,
       })
       .returning({ userId: users.id });
-    console.log('B');
-
     if (res.length !== 1) {
       return; // insertion has failed if no return
     }
-    console.log('C', getSixDigitId(res[0].userId), res[0].userId);
-
     // create display id
     await databaseClient.insert(userDisplayIds).values({
       userId: res[0].userId,
       displayId: getSixDigitId(res[0].userId),
     });
-    console.log('D');
   }),
   deleteUser: publicProcedure.input(deleteUserSchema).mutation(async (opts) => {
     await databaseClient.delete(users).where(eq(users.id, opts.input.id));
