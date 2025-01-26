b4982544302f773c553a6e065c4fd150b7abecce
Christina <christinaraganit@gmail.com>
2025-01-25 17:56:58 -0800

    layout for the login screen done, might need to compress the image to improve load times though

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,30 +5,58 @@ import { Button } from '@/components/ui/Button';
 
 export default function Login() {
     return (
-        <div id="auth" className="w-screen bg-neutral-925 h-screen p-6">
-            <div className="flex flex-col gap-4 justify-end h-full">
-                <Button
-                    variant="default"
-                    hierarchy="secondary"
-                    size="cozy"
-                    className="w-full"
-                    leadingIcon="/icons/google.svg"
-                    leadingIconAlt="Google logo"
-                >
-                    Continue with Google
-                </Button>
+        <div id="auth" className="md:grid md:grid-cols-2 2xl:grid-cols-3">
+            <div className="w-screen max-h-screen bg-neutral-925 h-screen p-6 flex flex-col gap-14 justify-center md:w-full 2xl:col-span-1">
+                <div className="flex flex-col gap-8 justify-center">
+                    <Image
+                        src="/login/sparkcheffrizz.png"
+                        width={80}
+                        height={80}
+                        className="rounded-lg mx-auto"
+                        alt="Sparky wearing a chef\'s hat"
+                    ></Image>
 
-                <Button
-                    variant="default"
-                    hierarchy="secondary"
-                    size="cozy"
-                    className="w-full"
-                    leadingIcon="/icons/github.svg"
-                    leadingIconAlt="GitHub logo"
-                >
-                    Continue with GitHub
-                </Button>
+                    <div className="text-center">
+                        <p className="font-semibold text-sm text-brand-400 mb-2">
+                            Welcome
+                        </p>
+                        <h1 className="text-3xl font-semibold text-white text-balance leading-tight">
+                            Sign in to the Surge portal
+                        </h1>
+                    </div>
+
+                    <div className="flex flex-col gap-4 *:max-w-96 items-center w-full">
+                        <Button
+                            variant="default"
+                            hierarchy="secondary"
+                            size="cozy"
+                            className="w-full"
+                            leadingIcon="/icons/google.svg"
+                            leadingIconAlt="Google logo"
+                        >
+                            Continue with Google
+                        </Button>
+
+                        <Button
+                            variant="default"
+                            hierarchy="secondary"
+                            size="cozy"
+                            className="w-full"
+                            leadingIcon="/icons/github.svg"
+                            leadingIconAlt="GitHub logo"
+                        >
+                            Continue with GitHub
+                        </Button>
+                    </div>
+                </div>
             </div>
+            <Image
+                src="/login/journeyhacks-header-2x.png"
+                alt="Stormy and Sparky are cooking."
+                width={1920}
+                height={1080}
+                className="hidden md:block h-full object-cover 2xl:col-span-2"
+            ></Image>
         </div>
     );
 }
