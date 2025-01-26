dd914aa4e8b22d5fcc63b94670a1bc0fb2a5ba64
goldentoaste <rayg2375@gmail.com>
2025-01-25 20:05:45 -0800

    bleh

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,6 +1,12 @@
+import { NextPage } from 'next';
 import { ReactNode } from 'react';
 
-export default function Layout({ children }: { children: ReactNode }) {
+export default function Layout({
+    children,
+    ...props
+}: {
+    children: ReactNode;
+}) {
     return (
         <>
             <h1>TODO REPLACE WITH HEADER FOOT NAVBARS ETC</h1>
