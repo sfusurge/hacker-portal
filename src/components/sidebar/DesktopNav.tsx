fa0fdfbd21cfdc36b73eacb92ab3f3b64a63cdc8
Christina <christinaraganit@gmail.com>
2025-01-26 23:49:23 -0800

    added styling for the container of the page content

--- src/components/sidebar/DesktopNav.tsx
+++ src/components/sidebar/DesktopNav.tsx
@@ -2,6 +2,9 @@ import clsx from 'clsx';
 import Image from 'next/image';
 import { NavLink } from './NavLink';
 import { HomeIcon } from '@heroicons/react/24/outline';
+import { UserGroupIcon } from '@heroicons/react/24/outline';
+import { CalendarDaysIcon } from '@heroicons/react/24/outline';
+import { BellAlertIcon } from '@heroicons/react/24/outline';
 
 interface DesktopNavProps {
     className?: string;
@@ -15,53 +18,94 @@ export default function DesktopNav({ className }: DesktopNavProps) {
                 className
             )}
         >
-            <div className="flex flex-col gap-5 h-full">
-                <div className="relative aspect-[5/3] rounded-xl overflow-hidden border border-neutral-800">
-                    <div className="absolute top-0 bg-neutral-900/50 backdrop-blur-lg flex flex-row gap-3 w-full p-2">
+            <div className="flex flex-col h-full justify-between">
+                <div className="flex flex-col gap-5">
+                    <div className="relative aspect-[5/3] rounded-xl overflow-hidden border border-neutral-800">
+                        <div className="absolute top-0 bg-neutral-900/50 backdrop-blur-lg flex flex-row gap-3 w-full p-2">
+                            <Image
+                                src="/login/sparkcheffrizz.png"
+                                alt="Sparky wearing a chef\'s hat"
+                                width={36}
+                                height={36}
+                                className="rounded-lg w-8 h-8 pointer-events-none"
+                            ></Image>
+
+                            <div className="flex flex-col gap-2">
+                                <span className="text-sm font-medium leading-none text-white">
+                                    JourneyHacks 2025
+                                </span>
+                                <span className="text-sm leading-none text-white/60">
+                                    February 14, 2025
+                                </span>
+                            </div>
+                        </div>
+
+                        <Image
+                            src="/sidebar/stormy-sparky-header.png"
+                            alt="Stormy and Sparky cooking"
+                            width={1080}
+                            height={1080}
+                            className="w-full h-full object-cover"
+                        ></Image>
+                    </div>
+
+                    <div className="links flex-1 flex flex-col gap-1">
+                        <NavLink
+                            href="#"
+                            label="Home"
+                            icon={<HomeIcon></HomeIcon>}
+                            iconAlt="Home logo"
+                            platform="desktop"
+                            active={true}
+                        ></NavLink>
+                        <NavLink
+                            href="#"
+                            label="Team"
+                            icon={<UserGroupIcon></UserGroupIcon>}
+                            iconAlt="Team logo"
+                            platform="desktop"
+                            active={false}
+                        ></NavLink>
+
+                        <NavLink
+                            href="#"
+                            label="Schedule"
+                            icon={<CalendarDaysIcon></CalendarDaysIcon>}
+                            iconAlt="Schedule logo"
+                            platform="desktop"
+                            active={false}
+                        ></NavLink>
+
+                        <NavLink
+                            href="#"
+                            label="Alerts"
+                            icon={<BellAlertIcon></BellAlertIcon>}
+                            iconAlt="Alerts logo"
+                            platform="desktop"
+                            active={false}
+                        ></NavLink>
+                    </div>
+                </div>
+
+                <div className="flex flex-row justify-between items-center">
+                    <div className="flex flex-row gap-3 items-center    ">
                         <Image
                             src="/login/sparkcheffrizz.png"
                             alt="Sparky wearing a chef\'s hat"
                             width={36}
                             height={36}
-                            className="rounded-lg w-8 h-8"
+                            className="rounded-full w-9 h-9"
                         ></Image>
 
                         <div className="flex flex-col gap-2">
-                            <span className="text-sm font-medium leading-none text-white">
-                                JourneyHacks 2025
+                            <span className="text-white font-medium text-base leading-none">
+                                Hacker name
                             </span>
-                            <span className="text-sm leading-none text-white/60">
-                                February 14, 2025
+                            <span className="text-white/60 text-sm leading-none">
+                                User type
                             </span>
                         </div>
                     </div>
-
-                    <Image
-                        src="/sidebar/stormy-sparky-header.png"
-                        alt="Stormy and Sparky cooking"
-                        width={1080}
-                        height={1080}
-                        className="w-full h-full object-cover"
-                    ></Image>
-                </div>
-
-                <div className="links flex-1 flex flex-col gap-1">
-                    <NavLink
-                        href="#"
-                        label="Home"
-                        icon={<HomeIcon></HomeIcon>}
-                        iconAlt="Home logo"
-                        platform="desktop"
-                        active={true}
-                    ></NavLink>
-                    <NavLink
-                        href="#"
-                        label="Home"
-                        icon={<HomeIcon></HomeIcon>}
-                        iconAlt="Home logo"
-                        platform="desktop"
-                        active={false}
-                    ></NavLink>
                 </div>
             </div>
         </div>
--- src/components/sidebar/NavLink.tsx
+++ src/components/sidebar/NavLink.tsx
@@ -13,19 +13,22 @@ interface NavLinkProps {
     iconAlt?: string;
 }
 
-const navLinkVariants = cva('flex items-center rounded-lg', {
-    variants: {
-        variant: {},
-        platform: {
-            desktop: 'h-11 flex-row text-base gap-3 px-3',
-            mobile: 'h-16 flex-col justify-center text-xs font-medium gap-2',
+const navLinkVariants = cva(
+    'group flex items-center rounded-lg transition-colors',
+    {
+        variants: {
+            variant: {},
+            platform: {
+                desktop: 'h-11 flex-row text-base gap-3 px-3',
+                mobile: 'h-16 flex-col justify-center text-xs font-medium gap-2',
+            },
+            active: {
+                true: 'text-white bg-brand-950 hover:bg-brand-900',
+                false: 'text-white/60 hover:bg-neutral-750/30',
+            },
         },
-        active: {
-            true: 'text-white bg-brand-900',
-            false: 'text-white/60',
-        },
-    },
-});
+    }
+);
 
 export function NavLink({
     className,
@@ -38,8 +41,8 @@ export function NavLink({
     ...props
 }: ComponentProps<'a'> & NavLinkProps & VariantProps<typeof navLinkVariants>) {
     const iconStyles = cn({
-        'text-brand-400': active && icon,
-        'text-white/30': !active && icon,
+        'text-brand-400 group-hover:text-brand-200': active && icon,
+        'text-white/30 group-hover:text-white/60': !active && icon,
     });
 
     return (
@@ -49,7 +52,9 @@ export function NavLink({
             className={cn(navLinkVariants({ className, platform, active }))}
         >
             {icon && iconAlt && (
-                <div className={cn('h-6 w-6', iconStyles)}>{icon}</div>
+                <div className={cn('h-6 w-6 transition-colors', iconStyles)}>
+                    {icon}
+                </div>
             )}
             <span className="leading-none">{label}</span>
         </Link>
