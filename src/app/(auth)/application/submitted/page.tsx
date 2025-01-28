f0989c77d2b5e935f69db400dd335b57042c78ae
Christina <christinaraganit@gmail.com>
2025-01-28 10:58:55 -0800

    added application submission page

--- src/app/(auth)/application/submitted/page.tsx
+++ src/app/(auth)/application/submitted/page.tsx
@@ -1,3 +1,31 @@
+import Image from 'next/image';
+import { Button } from '@/components/ui/button';
+
 export default function SubmitPage() {
-    return <h1 style={{ color: 'white' }}>Good job you submitted</h1>;
+    return (
+        <div className="flex flex-col h-full w-full items-center justify-center gap-8">
+            <Image
+                src="/login/application-review.png"
+                width={1537}
+                height={1134}
+                className="max-w-[280px]"
+                alt="Four otters are gathered around a table, reviewing application submissions."
+            ></Image>
+            <div className="text-center text-white">
+                <p className="font-semibold text-sm text-brand-400 mb-2">
+                    Submission Successful
+                </p>
+                <h1 className="text-3xl font-semibold text-white text-balance leading-tight mb-3">
+                    Thank you for applying to JourneyHacks 2025!
+                </h1>
+                <p className="text-base text-balance text-white/60">
+                    Keep an eye on your inbox for any updates regarding your
+                    application status.
+                </p>
+            </div>
+            <Button variant="brand" hierarchy="primary" size="cozy">
+                Return to home
+            </Button>
+        </div>
+    );
 }
