ff66ed2e520bd9d0bab39f80c84d94d02e4b7573
goldentoaste <rayg2375@gmail.com>
2025-01-28 12:33:01 -0800

    merged submit screen

--- src/app/(auth)/application/submitted/page.tsx
+++ src/app/(auth)/application/submitted/page.tsx
@@ -1,3 +1,32 @@
+import { SkewmorphicButton } from '@/components/ui/SkewmorphicButton/SkewmorphicButton';
+import style from 'styled-jsx/style';
+import Image from 'next/image';
+
 export default function SubmitPage() {
-    return <h1 style={{ color: 'white' }}>Good job you submitted</h1>;
+    return (
+        <div className="flex flex-col items-center justify-center gap-10">
+            <Image
+                src={'/login/team.svg'}
+                alt={'The Surge Team!'}
+                width={430}
+                height={317}
+            />
+            <div className="flex flex-col gap-1 justify-center items-center">
+                <h1 className="text-white font-sans text-3xl font-semibold">
+                    Application Submitted!
+                </h1>
+                <h2 className="text-white/60 text-sm font-sans">
+                    Your application has been submitted and will go under review
+                    by the JourneyHacks team soon.
+                </h2>
+            </div>
+
+            <SkewmorphicButton
+                onClick={returnHome}
+                className={style.nextButton}
+            >
+                Back to Dashboard
+            </SkewmorphicButton>
+        </div>
+    );
 }
