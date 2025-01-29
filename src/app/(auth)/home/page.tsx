0d660d3c42ea309eb02c69fef1284476236f4351
brendanshen24 <91840687+brendanshen24@users.noreply.github.com>
2025-01-28 17:09:40 -0800

    have the basic implementation of the application status changing based on whehter or not the user has submitted

--- src/app/(auth)/home/page.tsx
+++ src/app/(auth)/home/page.tsx
@@ -1,9 +1,23 @@
-import ApplicationCard from '@/components/home/ApplicationCard';
+import ApplicationCard, { AppStatus } from '@/components/home/ApplicationCard';
 import DiscordCard from '@/components/home/DiscordCard';
 import { getUserData } from '../layout';
+import { trpc } from '@/trpc/client';
+import { createCaller } from '@/server/appRouter';
 
 export default async function Home() {
     const data = await getUserData();
+    let status: AppStatus;
+    const trpcClient = createCaller({});
+
+    const applicationSubmitted =
+        await trpcClient.applications.userAlreadySubmitted({});
+
+    if (applicationSubmitted) {
+        status = 'Submitted – Under Review';
+    } else {
+        status = 'Not Yet Started';
+    }
+
     return (
         <div className="flex flex-col gap-6 md:gap-8">
             <h1 className="text-3xl text-white font-semibold">
@@ -11,7 +25,7 @@ export default async function Home() {
             </h1>
 
             <div className="grid gap-6 md:gap-8 pb-6 md:pb-0 xl:grid-cols-2">
-                <ApplicationCard status="In Progress"></ApplicationCard>
+                <ApplicationCard status={status}></ApplicationCard>
                 <DiscordCard></DiscordCard>
             </div>
         </div>
