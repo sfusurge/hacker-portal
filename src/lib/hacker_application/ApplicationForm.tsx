4e80146b0666c000e60e7b0e9f8e8b87fe98a7ef
goldentoaste <rayg2375@gmail.com>
2025-01-28 20:20:54 -0800

    WOWOWOWOWOW

--- src/lib/hacker_application/ApplicationForm.tsx
+++ src/lib/hacker_application/ApplicationForm.tsx
@@ -110,7 +110,7 @@ export function ApplicationForm({
     const [pageStateAtoms] = useAtom(pageStateAtomsAtom);
 
     // mobile conditional render
-    const isMobile = useMediaQuery('only screen and (max-width: 767px');
+    const isMobile = useMediaQuery('only screen and (max-width: 767.5px');
 
     return (
         <div className={style.appFormRoot}>
