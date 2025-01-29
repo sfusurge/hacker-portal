c0fcd88d0ccb538a7a98ff4a95ac2324f8155a67
Scott Lai <tuonglai3602@gmail.com>
2025-01-28 16:43:31 -0800

    feat: show application status

--- src/app/(auth)/home/page.tsx
+++ src/app/(auth)/home/page.tsx
@@ -1,9 +1,16 @@
 import ApplicationCard from '@/components/home/ApplicationCard';
 import DiscordCard from '@/components/home/DiscordCard';
 import { getUserData } from '../layout';
+import { createCaller } from '@/server/appRouter';
 
 export default async function Home() {
     const data = await getUserData();
+
+    const trpcClient = createCaller({});
+
+    const applicationSubmitted =
+        await trpcClient.applications.userAlreadySubmitted({});
+
     return (
         <div className="flex flex-col gap-6 md:gap-8">
             <h1 className="text-3xl text-white font-semibold">
@@ -11,7 +18,15 @@ export default async function Home() {
             </h1>
 
             <div className="grid gap-6 md:gap-8 pb-6 md:pb-0 xl:grid-cols-2">
-                <ApplicationCard status="In Progress"></ApplicationCard>
+                <ApplicationCard
+                    status={
+                        // TODO: return status of application and use that
+                        // status instead
+                        applicationSubmitted
+                            ? 'Submitted – Under Review'
+                            : 'In Progress'
+                    }
+                ></ApplicationCard>
                 <DiscordCard></DiscordCard>
             </div>
         </div>
