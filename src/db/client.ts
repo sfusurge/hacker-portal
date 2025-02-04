5d0aa40d3a3f4771566926a28f3af24c9f53996c
goldentoaste <rayg2375@gmail.com>
2025-02-04 15:23:49 -0800

    minor

--- src/db/client.ts
+++ src/db/client.ts
@@ -1,8 +1,6 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 
-const env = process.env;
-
 export const databaseClient = drizzle(
     postgres(
         process.env.DBURL ?? 'postgres://root:12345@localhost:5432/portaldb'
