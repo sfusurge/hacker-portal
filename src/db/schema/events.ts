333be5a20b48948da5bce22b2e3b4cdb021bfdd8
Scott Lai <tuonglai3602@gmail.com>
2025-02-08 14:17:39 -0800

    feat: add new tables, check_in and events

---
+++ src/db/schema/events.ts
@@ -0,0 +1,39 @@
+import {
+    index,
+    integer,
+    pgTable,
+    timestamp,
+    varchar,
+} from 'drizzle-orm/pg-core';
+import { createInsertSchema } from 'drizzle-zod';
+import { z } from 'zod';
+import { hackathons } from './hackathons';
+
+export const events = pgTable(
+    'events',
+    {
+        id: integer('id').generatedAlwaysAsIdentity().primaryKey(),
+        hackathonId: integer('hackathon_id')
+            .notNull()
+            .references(() => hackathons.id),
+        title: varchar('title', { length: 1024 }).notNull(),
+        color: varchar('color', { length: 128 }).notNull(),
+        startDate: timestamp('start_date').notNull(),
+        endDate: timestamp('end_date').notNull(),
+        location: varchar('location', { length: 1024 }).notNull(),
+        description: varchar('description', { length: 2048 }).default(''),
+    },
+    (table) => {
+        return [index().on(table.hackathonId)];
+    }
+);
+
+export const insertEventSchema = createInsertSchema(events);
+
+export const getEventsSchema = z.object({
+    hackathonId: z.number().int(),
+});
+
+export const deleteEventSchema = z.object({
+    id: z.number().int(),
+});
