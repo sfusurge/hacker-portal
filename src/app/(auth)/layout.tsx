26ec4e6e4c04581d300338777e23a47d6b27e79e
Tony Liu <tl0226yn@gmail.com>
2025-02-11 23:19:53 -0800

    change mobile breakpoint to 640px from 768px

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -61,20 +61,20 @@ export default async function Layout({ children }: { children: ReactNode }) {
     return (
         <>
             <div
-                className="bg-neutral-950  p-6 md:flex md:p-0 md:pr-5"
+                className="bg-neutral-950  p-6 sm:flex sm:p-0 sm:pr-5"
                 style={{ height: '100dvh' }}
             >
                 <CacheClearer initialData={initialUserData}></CacheClearer>
                 <MobileTopNav
                     initialData={initialUserData}
-                    className="top-0 left-0 fixed z-[100] md:hidden"
+                    className="top-0 left-0 fixed z-[100] sm:hidden"
                 ></MobileTopNav>
-                <MobileBottomNav className="bottom-0 left-0 fixed z-[100] md:hidden"></MobileBottomNav>
+                <MobileBottomNav className="bottom-0 left-0 fixed z-[100] sm:hidden"></MobileBottomNav>
                 <DesktopNav
                     initialData={initialUserData}
-                    className="hidden md:block"
+                    className="hidden sm:block"
                 ></DesktopNav>
-                <main className="mt-20 md:mt-20 pb-20 md:max-h-screen md:flex-1 md:bg-neutral-925 md:my-5 md:p-10 md:rounded-2xl md:border md:border-neutral-600/30 md:overflow-y-auto">
+                <main className="mt-20 sm:mt-20 pb-20 sm:max-h-screen sm:flex-1 sm:bg-neutral-925 sm:my-5 sm:p-10 sm:rounded-2xl sm:border sm:border-neutral-600/30 sm:overflow-y-auto">
                     {children}
                 </main>
             </div>
