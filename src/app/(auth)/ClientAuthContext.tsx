06ac69e25dc9131574611310ede929ae7473b85a
goldentoaste <rayg2375@gmail.com>
2025-02-13 18:10:19 -0800

    Update ClientAuthContext.tsx

--- src/app/(auth)/ClientAuthContext.tsx
+++ src/app/(auth)/ClientAuthContext.tsx
@@ -2,12 +2,15 @@
 
 import { atom, useSetAtom } from 'jotai';
 import { MergedUserData } from './layout';
+import { useLayoutEffect } from 'react';
 
 export const userInfoAtom = atom<MergedUserData | undefined>(undefined);
 
 export function ClientAuthContext({ userData }: { userData: MergedUserData }) {
     const setUserInfo = useSetAtom(userInfoAtom);
-    setUserInfo(userData);
+    useLayoutEffect(() => {
+        setUserInfo(userData);
+    }, []);
 
     return <></>;
 }
