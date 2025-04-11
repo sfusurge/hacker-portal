7d73db5336b87fecfbe3ff48de3b32f80aacb026
goldentoaste <rayg2375@gmail.com>
2025-04-10 21:22:06 -0600

    Other option for combobox (#81)
    * wip
    
    * allow "others" option in multi checkbox

--- src/app/(auth)/application/application_components/types.ts
+++ src/app/(auth)/application/application_components/types.ts
@@ -127,6 +127,8 @@ export interface QuestionMultipleCheckBox extends Question {
         name: string;
         value: boolean;
     }[];
+    allowOther?: boolean;
+    otherValue?: string;
 }
 
 /**
