2f662d3e7118f5d2ea184e1ad44d4cec10b0f484
goldentoaste <rayg2375@gmail.com>
2025-05-05 18:18:44 -0700

    Update ElementsForm.tsx

--- src/app/(auth)/test_rsvp/components/ElementsForm.tsx
+++ src/app/(auth)/test_rsvp/components/ElementsForm.tsx
@@ -97,7 +97,7 @@ function CheckoutForm({ userEmail }: { userEmail: string }) {
                 return;
             }
 
-            const paymentAmount = 15;
+            const paymentAmount = 0.5;
 
             const { client_secret: clientSecret } = await createPaymentIntent(
                 paymentAmount,
