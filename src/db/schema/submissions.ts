79937cb399cf4202ade6fab50624fb61e0c409a8
goldentoaste <rayg2375@gmail.com>
2025-05-22 21:39:10 -0700

    Update submissions.ts

--- src/db/schema/submissions.ts
+++ src/db/schema/submissions.ts
@@ -11,11 +11,9 @@ import { user } from './users/users';
 import { createInsertSchema } from 'drizzle-zod';
 import { z } from 'zod';
 
-export const submissionStatusEnum = pgEnum('application_status', [
-    'Winner',
-    'Loser',
-    'Finalist',
+export const submissionStatusEnum = pgEnum('submission_status', [
     'Awaiting Review',
+    'Reviewed',
 ]);
 
 export const submissions = pgTable(
