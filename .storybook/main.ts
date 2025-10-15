0f010a3e584728007893e9892ec1d2822bfb04e6
Philip <philip03ho@gmail.com>
2025-10-14 19:51:46 -0700

    change static directory import for storybook for vercel

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -7,6 +7,7 @@ const config: StorybookConfig = {
         name: '@storybook/nextjs',
         options: {},
     },
-    staticDirs: ['..\\public'],
+    staticDirs: ['../public'],
 };
+
 export default config;
